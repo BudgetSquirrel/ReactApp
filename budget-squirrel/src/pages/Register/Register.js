e2a07945c3417dc7cb30d863a6fbb3031146dff5
Ian Kirkpatrick <ianmann56@gmail.com>
2019-12-19 18:32:53 -0500

    Use a generic form template

--- budget-squirrel/src/pages/Register/Register.js
+++ budget-squirrel/src/pages/Register/Register.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import './Register.scss';
 import PasswordInput from '../../controls/PasswordInput/PasswordInput';
+import BasicForm from '../../components/generic/BasicForm/BasicForm';
 
 export default function Register() {
-    return (
-        <div class="register form">
-            <div>
-                <h2>Register your account</h2>
-                <p class="register__promo">
-                    Registering your account allows you to track
-                    your budget accross multiple platforms.
-                </p>
-            </div>
+    const title = "Register your account";
+    const description = "Registering your account allows you to track your budget accross multiple platforms.";
 
-            <div class="form__inputs register__inputs">
+    function RegisterInputs() {
+        return (
+            <>
                 <input class="input" type="text" placeholder="Name" />
                 <input class="input" type="text" placeholder="Email" />
                 <PasswordInput />
                 <p class="input__explain">Password must be at least 12 characters or longer.</p>
-            </div>
-
-            <div class="form__actions">
+            </>
+        )
+    }
+    
+    function RegisterButtons() {
+        return (
+            <>
                 <button class="button button--wide">Register</button>
-            </div>
+            </>
+        )
+    }
+
+    const inputs = <RegisterInputs />
+    const buttons = <RegisterButtons />
+
+    return (
+        <div class="register">
+            <BasicForm title={title} description={description}>
+                <RegisterInputs key="inputs" />
+                <RegisterButtons key="buttons" />
+            </BasicForm>
         </div>
     )
 }
