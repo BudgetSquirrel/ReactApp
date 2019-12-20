c6ce4a89233c72f1c05bc96432ade385b0796205
Ben Groseclose <benjamin.groseclose@hotmail.com>
2019-12-19 22:40:05 -0500

    [ch89] login page exists

--- README.md
+++
@@ -1,7 +0,0 @@
-# ReactApp
-React Frontend for BudgetSquirrel
-
-## Getting Started
-
-- `npm install`
-- `npm start` in the `budget_squirrel` folder
--- budget-squirrel/src/pages/Login/Login.js
+++ budget-squirrel/src/pages/Login/Login.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import './Login.scss'
+import BasicForm from '../../components/generic/BasicForm/BasicForm';
+import PasswordInput from '../../controls/PasswordInput/PasswordInput';
 
 export default function Login() {
+
+    const title = "Login";
+    const description = "Login in your account to view your budget details";
+
+    function LoginInput() {
+        return (
+            <>
+                <input class="input" type="text" placeholder="Username" />
+                <PasswordInput />                
+            </>
+        )
+    }
+
+    function LoginButton() {
+        return (
+            <>
+                <button class="button button--wide">Login</button>
+            </>
+        )
+    }
+
     return (
         <div class="login">
-            
+            <BasicForm title={title} description={description}>
+                <LoginInput key="inputs" />
+                <LoginButton key="buttons" />
+            </BasicForm>
         </div>
     )
 }
